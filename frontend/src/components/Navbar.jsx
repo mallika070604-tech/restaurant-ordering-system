4e22bf26637e00f6ecfe10554fcd4c86b164f1a3
mallika070604-tech <mallika070604@gmail.com>
2026-06-21 10:40:27 +0530

    Remove kitchen and admin links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,12 +6,6 @@ export default function Navbar() {
       <Link to="/" className="btn-secondary text-xs sm:text-sm">
         Home
       </Link>
-      <Link to="/kitchen" className="btn-secondary text-xs sm:text-sm">
-        Kitchen
-      </Link>
-      <Link to="/admin" className="btn-secondary text-xs sm:text-sm">
-        Admin
-      </Link>
     </nav>
   );
 }
